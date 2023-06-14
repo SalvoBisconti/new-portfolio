11724bfc81a03c0eda3bdd1e28d3402c86ca1dc8
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-06-15 00:24:28 +0200

    fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const onHandleClick = (): void => {
     setOpenMenu((prev) => !prev);
   };
-
+  console.log(openMenu);
   return (
     <div className="h-[80px] z-10 bg-[#100c08] fixed w-screen p-3  flex justify-between items-center shadow-sm shadow-[#ff6961]">
       <h2 className="font-mono">
@@ -15,14 +15,28 @@ const Navbar = () => {
         Salvatore Bisconti <span className="text-lime-500"> {"/>"} </span>
       </h2>
       <button
-        className="relative group md:hidden z-20 "
+        className="relative group md:hidden z-30 "
         onClick={onHandleClick}
       >
-        <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[40px] h-[40px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
-          <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
-            <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg]"></div>
-            <div className="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-translate-x-10"></div>
-            <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg]"></div>
+        <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[44px] h-[44px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+          <div
+            className={`flex flex-col justify-between w-[24px] h-[24px] transform transition-all duration-300 origin-center overflow-hidden`}
+          >
+            <div
+              className={`bg-[#ff6961] h-[3px] w-8 transform transition-all duration-300 origin-left ${
+                openMenu && "rotate-[42deg]"
+              }`}
+            ></div>
+            <div
+              className={`bg-[#ff6961] h-[3px] w-1/2 rounded transform transition-all duration-300  0 ${
+                openMenu && "hidden"
+              } `}
+            ></div>
+            <div
+              className={`bg-[#ff6961] h-[3px] w-8 transform transition-all duration-300 origin-left ${
+                openMenu && "-rotate-[42deg]"
+              }`}
+            ></div>
           </div>
         </div>
       </button>
@@ -39,8 +53,8 @@ const Navbar = () => {
 
       <div
         className={` ${
-          openMenu ? " right-0  delay-75 " : "h-0 w-0    "
-        } z-10 fixed h-screen w-screen  bg-[#161a1d] top-[0px] right-[-1000px] p-3  flex justify-center items-center md:hidden  transition-all duration-500`}
+          openMenu ? " right-[0px]  delay-75 " : "h-0 w-0    "
+        } z-20 fixed h-screen w-screen  bg-[#161a1d] top-[0px] right-[-1000px] p-3  flex justify-center items-center md:hidden  transition-all duration-500`}
       >
         <List
           display={` ${openMenu ? "block" : "hidden"} `}
