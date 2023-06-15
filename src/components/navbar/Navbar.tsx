76bbaab33d71928abaef5bd51619357b63e69dc9
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-06-15 05:25:22 +0200

    fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,7 +25,8 @@ const Navbar = () => {
   return (
     <div
       className={`h-[80px] z-10 bg-trasparent fixed w-screen p-3  flex justify-between items-center transition-all duration-700 ${
-        scroll > 0 && "h-[50px] bg-[#100c08] shadow-sm shadow-[#ff6961] "
+        scroll > 0 &&
+        " absolute h-[40px] bg-[#100c08] md:shadow-sm md:shadow-[#ff6961] md:fixed "
       }`}
     >
       <h2 className="font-mono ">
@@ -33,19 +34,17 @@ const Navbar = () => {
         Salvatore Bisconti <span className="text-white"> {"/>"} </span>
       </h2>
       <button
-        className="relative group md:hidden z-30 "
+        className={` group md:hidden z-30 ${
+          scroll > 0 && "fixed top-4 right-6"
+        }`}
         onClick={onHandleClick}
       >
         <div
           className={`relative flex overflow-hidden items-center justify-center rounded-full w-[48px] h-[48px] transform transition-all bg-slate-800  ring-0 ring-gray-300 hover:ring-4 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md ${
-            scroll > 0 && "w-[36px] h-[36px]"
+            scroll > 0 && " ring-4 ring-opacity-30 "
           } `}
         >
-          <div
-            className={`flex flex-col justify-between w-[24px] h-[24px] transform transition-all duration-300 origin-center overflow-hidden ${
-              scroll > 0 && " w-[19px] h-[19px]"
-            } `}
-          >
+          <div className="flex flex-col justify-between w-[24px] h-[24px] transform transition-all duration-300 origin-center overflow-hidden ">
             <div
               className={`bg-[#ff6961] h-[3px] w-8 transform transition-all duration-300 origin-left ${
                 openMenu && "rotate-[42deg]"
