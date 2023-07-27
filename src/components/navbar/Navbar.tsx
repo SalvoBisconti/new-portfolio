5a6853bf7334200cf49347eb815ce65985810886
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-27 21:24:12 +0200

    fix

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -44,25 +44,26 @@ const Navbar = () => {
         onClick={onHandleClick}
       >
         <div
-          className={`relative flex overflow-hidden items-center justify-center rounded-full w-[48px] h-[48px] transform transition-all bg-second ring-0 ring-gray-300 hover:ring-4 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md ${
-            scroll > 0 && " ring-4 ring-opacity-30 "
+          className={`relative flex overflow-hidden items-center justify-center rounded-full w-[48px] h-[48px] transform transition-all bg-[#161a1d] ring-0 ring-gray-300 hover:ring-4 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md ${
+            scroll > 0 && " ring-4 ring-opacity-30 bg-second "
           } `}
         >
           <div className="flex flex-col justify-between w-[24px] h-[24px] transform transition-all duration-300 origin-center overflow-hidden ">
             <div
-              className={`bg-[#161a1d] h-[4px] w-9 transform transition-all duration-300 origin-left  ${
+              className={`bg-second h-[3px] w-9 transform transition-all duration-300 origin-left  ${
                 openMenu && "rotate-[42deg]"
-              }`}
+              } ${scroll > 0 && "bg-black h-[4px]"}
+              `}
             ></div>
             <div
-              className={`bg-[#161a1d] h-[4px] w-2/3 rounded transform transition-all duration-300  0 ${
+              className={`bg-second h-[3px] w-2/3 rounded transform transition-all duration-300  0 ${
                 openMenu && "hidden"
-              } `}
+              } ${scroll > 0 && "bg-black h-[4px] "}`}
             ></div>
             <div
-              className={`bg-[#161a1d] h-[4px] w-8 transform transition-all duration-300 origin-left ${
+              className={`bg-second h-[3px] w-8 transform transition-all duration-300 origin-left ${
                 openMenu && "-rotate-[42deg]"
-              }`}
+              } ${scroll > 0 && "bg-black h-[4px]"}`}
             ></div>
           </div>
         </div>
