4aca20961054309590aa0699239265699a968896
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-27 18:10:56 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Salvatore Bisconti - Front-end developer"
         />
         <meta name="robots" content="index,nofollow" />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#161a1d" />
         <link rel="icon" type="image/x-icon" href="favicon.ico" />
       </Head>
