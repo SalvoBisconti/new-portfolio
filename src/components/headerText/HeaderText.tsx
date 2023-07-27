2851e7e537dc112c744821dfcae78cf26e098f7d
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-28 00:41:12 +0200

    fix

--- src/components/headerText/HeaderText.tsx
+++ src/components/headerText/HeaderText.tsx
@@ -1,6 +1,11 @@
 const HeaderText = (props: { text: string }) => {
   const { text } = props;
-  return <h2 className="text-center p-4 font-bold text-2xl m-6 "> {text}</h2>;
+  return (
+    <h2 className="text-center p-4 font-bold text-2xl md:text-3xl m-6 [text-shadow:_0_1px_0_rgb(255_255_255_/_50%)] ">
+      {" "}
+      {text}
+    </h2>
+  );
 };
 
 export default HeaderText;
