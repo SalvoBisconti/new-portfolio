3fd874f21d53b465d8ee0fb7ee5c79ab92695f66
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-27 20:41:43 +0200

    fix

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -76,7 +76,7 @@ const Hero = () => {
           </li>
         </ul>
       </AnimationOnScroll>
-      <h2 className="absolute bottom-[44vw] -right-[1vw] -rotate-[38deg] text-[20vw] -z-20 text-[#36454F] md:bottom-[26vw] md:right-[4vw] md:text-[150px] xl:right-[10vw] xl:bottom-[16vw] xl:text-[230px] xl:-rotate-[25deg] transition-all">
+      <h2 className="absolute bottom-[44vw] -right-[5vw] -rotate-[45deg] text-[22vw] -z-20 text-[#36454F] md:bottom-[26vw] md:right-[4vw] md:text-[150px] xl:right-[10vw] xl:bottom-[16vw] xl:text-[230px] xl:-rotate-[25deg] transition-all">
         {" "}
         DEVELOPER
       </h2>
